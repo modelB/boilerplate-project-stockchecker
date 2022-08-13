6d1177717898e48b5eceae9649ba334d69563c73
Brian Grosso <bgro63@gmail.com>
2022-08-12 23:14:01 -0400

    rough res

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,16 @@
-'use strict';
+"use strict";
+
+const { default: axios } = require("axios");
 
 module.exports = function (app) {
-
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+  app.route("/api/stock-prices").get(async function (req, res) {
+    const { stock, like } = req.query;
+    if (stock) {
+      const stockPriceRes = (await axios.get(
+        `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+      )).data;
+      console.log(stockPriceRes);
+      res.status(200).send({stockData: { stock: stock.toUpperCase(), price: stockPriceRes.latestPrice, likes: 0 }});
+    } else res.send(400);
+  });
 };
